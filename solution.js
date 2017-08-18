b6be416280790c81da23c07da4cc97cb24812fd2
Sergey Serdyuk <fxduke@gmail.com>
2017-08-18 23:52:01 +0300

    I did it

--- solution.js
+++ solution.js
@@ -1,15 +1,43 @@
 function drawNestedSetsTree(data, node) {
-  // Удалите весь код ниже и замените его на свою реализацию
   const ul = document.createElement('ul');
-  data.forEach(({ title }) => {
-    const li = document.createElement('li');
-    li.textContent = title;
-    ul.appendChild(li);
+
+  function nodeHasChildren(node) {
+    return node.right - node.left == 1 ? false : true;
+  }
+
+  function getChildren(arr, node) {
+    const children = [];
+    let childIndex = node.left + 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].left == childIndex) {
+        children.push(arr[i]);
+        childIndex = arr[i].right + 1;
+      }
+    }
+    return children;
+  }
+
+  // sort data by { left }
+  data.sort((objA, objB) => {
+    return objA.left - objB.left;
   });
+
+  const addNodesInHTML = (arr, nodes) => {
+    return nodes.map((node) => {
+      if (nodeHasChildren(node)) {
+        const children = getChildren(arr, node);
+        if (children.length > 0) {
+          return `<li>${node.title}<ul>${addNodesInHTML(arr, children)}</ul></li>`;
+        }
+      } else {
+        return `<li>${node.title}</li>`;
+      }
+    }).join('');
+  };
+
+  ul.innerHTML = addNodesInHTML(data, [data[0]]);
   node.appendChild(ul);
 }
-
-
 
 if (typeof module !== 'undefined') {
   module.exports = drawNestedSetsTree;
